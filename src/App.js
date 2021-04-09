1a5fa16b07fa92cc77dd1dc707379cb851eeee07
Federico Giovannini <giovannini.federico@hotmail.it>
2021-04-09 20:00:59 +0200

    new Screens

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Mail from "./Screens/Mail";
+import EmailList from "./Screens/EmailList";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Sidebar />
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
