f486a03e6521ff80a29680f15cbf8083e7116502
Federico Giovannini <giovannini.federico@hotmail.it>
2021-04-10 20:17:52 +0200

    Mail body

--- src/Screens/EmailList.js
+++ src/Screens/EmailList.js
@@ -1,8 +1,58 @@
 import React from "react";
+import { Checkbox, IconButton } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+import SettingsIcon from "@material-ui/icons/Settings";
+import InboxIcon from "@material-ui/icons/Inbox";
+import PeopleIcon from "@material-ui/icons/People";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import "./EmailList.css";
+import Section from "../components/Section";
 
 function EmailList() {
-  return <div className="emailList"></div>;
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+
+        <div className="emailList__settingRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+
+        <div className="emailList__sections">
+          <Section Icon={InboxIcon} title="primary" color="red" selected />
+          <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+          <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EmailList;
